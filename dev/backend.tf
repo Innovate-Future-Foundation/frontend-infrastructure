3c686d62b214d412f53c7e39ed3dde80ee1e7457
LingHan.S <LINGKUI0324@GMAIL.COM>
2025-01-21 23:15:47 +1000

    Remove Terraform configuration and related resources for frontend infrastructure

--- dev/backend.tf
+++
@@ -1,9 +0,0 @@
-terraform {
-  backend "s3" {
-    bucket         = "inff-frontend-infrastructure-tfstate-xxxxx" # todo
-    key            = "dev/terraform.tfstate"
-    region         = "ap-southeast-2"  
-    dynamodb_table = "inff-frontend-infrastructure-tflock"# todo
-    encrypt        = true
-  }
-}
--- dev/route53_acm/outputs.tf
+++
@@ -1,7 +0,0 @@
-output "certificate_arn" {
-  value = aws_acm_certificate.frontend.arn
-}
-
-output "domain_zone_id" {
-  value = data.aws_route53_zone.domain.zone_id
-}
